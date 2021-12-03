a386701c95aad19b2fe779322480841604f18669
meridiani <maria.duffy@physics.org>
2021-12-03 19:13:04 +0000

    Add script to generate files for a new day

---
+++ templates/day_template.py
@@ -0,0 +1,11 @@
+import utils
+
+DATA = 'data/dayXX/realdata'
+
+input = utils.load_data(DATA)
+
+print('The first answer is:', 'something')
+print('The second answer is:', 'something')
+
+#######################################################################
+print('OK to go!')
