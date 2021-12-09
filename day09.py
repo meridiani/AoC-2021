f794399aaebb3ea6e94a66aa72bbf6653f6c4f16
meridiani <maria.duffy@physics.org>
2021-12-09 09:20:04 +0000

    Day 9: Smoke Basin

---
+++ day09.py
@@ -0,0 +1,11 @@
+import utils
+
+DATA = 'data/day09/realdata'
+
+input = utils.load_data(DATA)
+
+print('The first answer is:', 'something')
+print('The second answer is:', 'something')
+
+#######################################################################
+print('OK to go!')
