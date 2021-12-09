092e9c51ff988dd7b00fd7b565c2d6530c7a4ffe
Maria Duffy <maria.duffy@dvsa.gov.uk>
2021-12-09 14:47:30 +0000

    finished part 1

--- day09.py
+++ day09.py
@@ -1,8 +1,8 @@
 import utils
 import sys
 
-#DATA = 'data/day09/realdata'
-DATA = 'data/day09/testdata'
+DATA = 'data/day09/realdata'
+#DATA = 'data/day09/testdata'
 
 input = utils.load_string_data(DATA)
 
@@ -20,10 +20,10 @@ max_y = len(heightmap)
 # at the end sum the low_points and add the length of the list to find the sum of the risk levels
 # the corners and sides are calculated separately as they are surrounded by less than four numbers
 
-def check_loc_for_low_point(my_list):
-    is_low_point = False
-
-    return is_low_point
+def check_loc_for_low_point(low_list, my_list):
+    if check_loc_is_unique(my_list) and check_loc_is_min(my_list):
+        low_list.append(my_list[0])
+    return low_list
 
 def check_loc_is_unique(my_list):
     if my_list.count(my_list[0]) == 1:
@@ -44,37 +44,43 @@ for x in range(max_x):
         if x == 0 and y == 0:
             # top left
             loc_list = [loc, heightmap[y+1][x], heightmap[y][x+1]]
-            if check_loc_is_unique(loc_list) and check_loc_is_min(loc_list):
-                low_points.append(loc)
-            print(loc_list)
+            check_loc_for_low_point(low_points, loc_list)
         elif x == 0 and y == max_y - 1:
             # bottom left
             loc_list = [loc, heightmap[y-1][x], heightmap[y][x+1]]
-            if check_loc_is_unique(loc_list) and check_loc_is_min(loc_list):
-                low_points.append(loc)
-            print(loc_list)
+            check_loc_for_low_point(low_points, loc_list)
         elif x == max_x - 1 and y == 0:
             # top right
             loc_list = [loc, heightmap[y][x-1], heightmap[y+1][x]]
-            if check_loc_is_unique(loc_list) and check_loc_is_min(loc_list):
-                low_points.append(loc)
-            print(loc_list)
+            check_loc_for_low_point(low_points, loc_list)
         elif x == max_x - 1 and y == max_y - 1:
             # bottom right
             loc_list = [loc, heightmap[y-1][x], heightmap[y][x-1]]
-            if check_loc_is_unique(loc_list) and check_loc_is_min(loc_list):
-                low_points.append(loc)
-            print(loc_list)
+            check_loc_for_low_point(low_points, loc_list)
+        # now find edges
+        elif x == 0:
+            # left
+            loc_list = [loc, heightmap[y-1][x], heightmap[y+1][x], heightmap[y][x+1]]
+            check_loc_for_low_point(low_points, loc_list)
+        elif x == max_x -1:
+            # right
+            loc_list = [loc, heightmap[y-1][x], heightmap[y+1][x], heightmap[y][x-1]]
+            check_loc_for_low_point(low_points, loc_list)
+        elif y == 0:
+            # top
+            loc_list = [loc, heightmap[y][x-1], heightmap[y+1][x], heightmap[y][x+1]]
+            check_loc_for_low_point(low_points, loc_list)
+        elif y == max_y -1:
+            # bottom
+            loc_list = [loc, heightmap[y][x-1], heightmap[y-1][x], heightmap[y][x+1]]
+            check_loc_for_low_point(low_points, loc_list)
         else:
-            continue
+            # must be in the middle
+            loc_list = [loc, heightmap[y-1][x], heightmap[y][x+1], heightmap[y+1][x], heightmap[y][x-1]]
+            check_loc_for_low_point(low_points, loc_list)
 
-print(low_points)
-
-
-
-
-print('The first answer is:', 'something')
+#######################################################################
+print('The first answer is:', (sum(low_points) + len(low_points)))
 print('The second answer is:', 'something')
-
 #######################################################################
 print('OK to go!')
