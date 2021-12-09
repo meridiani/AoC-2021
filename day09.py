1076a8b722a377593a719511bce18628e96f8c90
Maria Duffy <maria.duffy@dvsa.gov.uk>
2021-12-09 09:23:06 +0000

    wip

--- day09.py
+++ day09.py
@@ -1,8 +1,9 @@
 import utils
 
-DATA = 'data/day09/realdata'
+#DATA = 'data/day09/realdata'
+DATA = 'data/day09/testdata'
 
-input = utils.load_data(DATA)
+input = utils.load_string_data(DATA)
 
 print('The first answer is:', 'something')
 print('The second answer is:', 'something')
